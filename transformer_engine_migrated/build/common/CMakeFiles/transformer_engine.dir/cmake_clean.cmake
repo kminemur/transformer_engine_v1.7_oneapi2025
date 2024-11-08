cbeafdd4be07e60d3d3ff39a7ba122520c527276
kazuki minemura <kazuki.minemura@intel.com>
2024-11-08 18:40:24 +0900

    fixed

---
+++ transformer_engine_migrated/build/common/CMakeFiles/transformer_engine.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transformer_engine.dir/fused_attn/fused_attn.cpp.o"
+  "CMakeFiles/transformer_engine.dir/fused_attn/fused_attn.cpp.o.d"
+  "CMakeFiles/transformer_engine.dir/layer_norm/ln_api.cpp.o"
+  "CMakeFiles/transformer_engine.dir/layer_norm/ln_api.cpp.o.d"
+  "CMakeFiles/transformer_engine.dir/rmsnorm/rmsnorm_api.cpp.o"
+  "CMakeFiles/transformer_engine.dir/rmsnorm/rmsnorm_api.cpp.o.d"
+  "CMakeFiles/transformer_engine.dir/transformer_engine.cpp.o"
+  "CMakeFiles/transformer_engine.dir/transformer_engine.cpp.o.d"
+  "CMakeFiles/transformer_engine.dir/util/cuda_driver.cpp.o"
+  "CMakeFiles/transformer_engine.dir/util/cuda_driver.cpp.o.d"
+  "CMakeFiles/transformer_engine.dir/util/cuda_runtime.cpp.o"
+  "CMakeFiles/transformer_engine.dir/util/cuda_runtime.cpp.o.d"
+  "CMakeFiles/transformer_engine.dir/util/rtc.cpp.o"
+  "CMakeFiles/transformer_engine.dir/util/rtc.cpp.o.d"
+  "CMakeFiles/transformer_engine.dir/util/system.cpp.o"
+  "CMakeFiles/transformer_engine.dir/util/system.cpp.o.d"
+  "libtransformer_engine.pdb"
+  "libtransformer_engine.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transformer_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
