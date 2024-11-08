7f1da2b1fe202a6213791ab9317fc96a1e59a743
kazuki minemura <kazuki.minemura@intel.com>
2024-11-08 18:43:37 +0900

    fixed

--- transformer_engine_migrated/build/common/string_headers/string_path_cuda_include.h
+++
@@ -1,8 +0,0 @@
-/*************************************************************************
- * Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- *
- * See LICENSE for license information.
- ************************************************************************/
-
-static constexpr char string_path_cuda_include[]
-  = R"__STRING_DELIM__(NOTFOUND)__STRING_DELIM__";
